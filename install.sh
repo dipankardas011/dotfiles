38e1676ef971843fc2c7af146f5489e41e535c82
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2025-08-10 00:22:13 +0530

    updated the code for seting up hyprland and wofi and waybar
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -65,6 +65,42 @@ function setup_alacritty() {
     ln -s "$LINUX_CONFIG_DIR/alacritty" "$ALACRITTY_DIR"
 }
 
+function setup_hyprland() {
+    local HYPRLAND_DIR="$HOME/.config/hypr"
+
+    if [ ! -d "$HYPRLAND_DIR" ]; then
+        mkdir -p "$HYPRLAND_DIR"
+    else
+        rm -rf "$HYPRLAND_DIR"
+    fi
+
+    ln -s "$LINUX_CONFIG_DIR/hypr" "$HYPRLAND_DIR"
+}
+
+function setup_waybar() {
+    local WAYBAR_DIR="$HOME/.config/waybar"
+
+    if [ ! -d "$WAYBAR_DIR" ]; then
+        mkdir -p "$WAYBAR_DIR"
+    else
+        rm -rf "$WAYBAR_DIR"
+    fi
+
+    ln -s "$LINUX_CONFIG_DIR/waybar" "$WAYBAR_DIR"
+}
+
+function setup_wofi() {
+    local WOFI_DIR="$HOME/.config/wofi"
+
+    if [ ! -d "$WOFI_DIR" ]; then
+        mkdir -p "$WOFI_DIR"
+    else
+        rm -rf "$WOFI_DIR"
+    fi
+
+    ln -s "$LINUX_CONFIG_DIR/wofi" "$WOFI_DIR"
+}
+
 function main() {
     if [ "$(uname)" != "Linux" ]; then
         echo "This script is intended for Linux systems only."
@@ -75,6 +111,10 @@ function main() {
     setup_neovim
     setup_tmux
     setup_alacritty
+    setup_hyprland
+    setup_waybar
+    setup_wofi
+    echo "All configurations have been set up successfully."
 }
 
 main "$@"
