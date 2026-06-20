cef93027aee287d10578904d591ef6aebb408d02
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2026-06-20 16:31:52 +0530

    updated the problem of dark mode in the gnome gtk apps
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

---
+++ problem-with-darkmode.sh
@@ -0,0 +1,7 @@
+# This is for the new fedora 44
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Adw-gtk3-dark'
+sudo dnf install lxappearance adw-gtk3-theme -y
+
